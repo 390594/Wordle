a8ad75b4c5e37cd4b2890f590e073615b1acd423
Daniel Hoskins <daniel.hoskins@mrjaffesclass.com>
2023-01-26 14:11:49 -0800

    1/26

---
+++ Wordle.java
@@ -0,0 +1,16 @@
+
+/**
+ * Wordle in a terminal
+ *
+ * @ By Daniel Hoskins
+ * @version (1/26/23)
+ */
+public class Wordle
+{
+    public static void main (String[] args)
+    {
+        System.out.println("WORDLE");
+        
+        
+    }
+}
