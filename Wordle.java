4d33d841d01ec14e32c7e233f28992dfd39c7290
Daniel Hoskins <daniel.hoskins@mrjaffesclass.com>
2023-01-30 14:24:08 -0800

    1/30

--- Wordle.java
+++ Wordle.java
@@ -12,31 +12,37 @@ public class Wordle
 {
     public static void main (String[] args)
     {
-        //Making the background of each letter
+        //Making the background of each letter with these color codes
         final String BG_GREEN = "\u001b[32m";
         final String BG_YELLOW = "\u001b[33m";
         final String RESET = "\u001b[0m";
 
-        System.out.println("WORDLE");
+        System.out.println("WORDLE!");
         String correct = "SHAKE";
-        String guess = "SPADE";
 
-        // Making a loof to compare through each letter
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Please guess ---> ");
+        String guess;
+        guess = sc.nextLine();
+        guess =
+
+        // Making a loop to compare through each letter
         //it's 5 because there's 5 letters in wordle
         for (int i=0;i<5;i++) 
         {
-            if (guess.substring(i,i+1) == (correct.substring(0,1))) {
+            if (guess.substring(i,i+1) == (correct.substring(i,i+1))) {
 
-                //if the letters match
+                //if the letters match, green highlight
                 System.out.print(BG_GREEN + guess.substring(0,1) + RESET);
                 //index identifies where a leter is in a different string
                 //if it's not there it will return a -1
             } else if (correct.indexOf(guess.substring (i,i+1)) > -1) {
                 //if letter is in word but in different spot,make it yellow 
                 System.out.print(BG_YELLOW + guess.substring(i,i+1) + RESET); 
-            } else  {
+            } else {
                 // if letter not found in the word, nothing happens, no color
-                System.out.print(guess.substring(0,1));
+                System.out.print(guess.substring(i,i+1));
 
             }
         }
